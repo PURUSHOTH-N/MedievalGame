273cf64cb987d6ac536fec33db6e282f5a7939f1
PURUSHOTH-N <purushothn118@gmail.com>
2026-02-27 18:13:02 +0530

    Added Trouser and Shoe subclasses

--- src/game/Shoe.java
+++ src/game/Shoe.java
@@ -1,5 +1,8 @@
 package game;
 
-public class Shoe {
+public class Shoe extends Armour {
 
+	public Shoe(String name, int durability, int defenseRating) {
+		super(name, durability, defenseRating);
+	}
 }
--- src/game/Trouser.java
+++ src/game/Trouser.java
@@ -1,5 +1,8 @@
 package game;
 
-public class Trouser {
+public class Trouser extends Armour {
 
+	public Trouser(String name, int durability, int defenseRating) {
+		super(name, durability, defenseRating);
+	}
 }
