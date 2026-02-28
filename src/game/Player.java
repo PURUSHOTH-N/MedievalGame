14ea43019643fd0ae2291ab81189333f7984171a
PURUSHOTH-N <purushothn118@gmail.com>
2026-02-28 20:08:59 +0530

    Implemented basic Player class with default weapon

--- src/game/Player.java
+++ src/game/Player.java
@@ -2,4 +2,28 @@ package game;
 
 public class Player {
 
+    private String name;
+    private int health;
+    private Weapon weapon;
+
+    public Player(String name) {
+        this.name = name;
+        this.health = 100;
+        this.weapon = new Weapon("Rusty Short Sword", 5);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    @Override
+    public String toString() {
+        return "Player: " + name +
+               "\nHealth: " + health +
+               "\nWeapon: " + weapon.getName();
+    }
 }
