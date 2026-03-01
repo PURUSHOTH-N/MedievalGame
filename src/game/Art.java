a4e0a69f619b55721b193517000525c42c0412bd
PURUSHOTH-N <purushothn118@gmail.com>
2026-03-01 19:01:01 +0530

    Added ASCII Art home screen

--- src/game/Art.java
+++ src/game/Art.java
@@ -2,4 +2,9 @@ package game;
 
 public class Art {
 
+	public static void homeScreen() {
+		System.out.println("=================================");
+		System.out.println("     WELCOME TO MEDIEVAL RPG     ");
+		System.out.println("=================================");
+	}
 }
