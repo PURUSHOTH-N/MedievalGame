238a9d3e17edfa787806b5c6a07a32a317bf4f25
PURUSHOTH-N <purushothn118@gmail.com>
2026-03-02 20:18:44 +0530

    Added start method structure

--- src/game/MedievalGame.java
+++ src/game/MedievalGame.java
@@ -1,5 +1,16 @@
 package game;
 
+import java.util.Scanner;
+
 public class MedievalGame {
 
+	public Player start(Scanner console) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		MedievalGame game = new MedievalGame();
+		game.start(console);
+	}
 }
