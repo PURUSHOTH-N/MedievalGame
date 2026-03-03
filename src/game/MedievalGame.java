fcd8c8da661d1acbc2be37adf1ef2fd1fddaba0e
PURUSHOTH-N <purushothn118@gmail.com>
2026-03-03 21:55:50 +0530

    Added save functionality

--- src/game/MedievalGame.java
+++ src/game/MedievalGame.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
 public class MedievalGame {
@@ -12,5 +15,23 @@ public class MedievalGame {
 		Scanner console = new Scanner(System.in);
 		MedievalGame game = new MedievalGame();
 		game.start(console);
+	}
+
+	private Player player;
+
+	public void save() {
+		try {
+			FileOutputStream fos = new FileOutputStream(player.getName() + ".svr");
+
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+			oos.writeObject(player);
+
+			oos.close();
+			fos.close();
+
+		} catch (IOException e) {
+			System.out.println("Save failed.");
+		}
 	}
 }
